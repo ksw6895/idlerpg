75a9ece2d1b5b239142996fd4a92f726947f594f
ksw6895 <ksw6895@gmail.com>
2025-09-19 17:52:53 +0900

    Fix Phaser CDN import syntax

--- public/src/phaser.js
+++ public/src/phaser.js
@@ -1,3 +1,3 @@
-import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.esm.js';
+import * as Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.60.0/dist/phaser.esm.js';
 
 export default Phaser;
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lumin-grove-v3';
+const CACHE_NAME = 'lumin-grove-v4';
 const CORE_ASSETS = [
   '/',
   '/index.html',
